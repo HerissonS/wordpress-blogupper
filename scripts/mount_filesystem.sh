84a71b70b293d245e237475bf274ffc068a7e95a
HerissonS <antonioherisson@gmail.com>
2024-09-18 22:05:20 -0300

    Atualizacao script para appspec.yml

--- scripts/mount_filesystem.sh
+++ scripts/mount_filesystem.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # Executar o comando mount -a para montar os sistemas de arquivos
-mount -a
 rm -rf /var/www/html/index.html
 chown -R www-data:www-data /var/www/html
 chmod -R 755 /var/www/html
 echo "Permissões ajustadas para www-data."
+mount -a
