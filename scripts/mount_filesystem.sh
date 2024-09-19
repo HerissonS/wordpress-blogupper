89052bc17f285f12adce69202ac1cc205222a969
HerissonS <antonioherisson@gmail.com>
2024-09-18 22:59:53 -0300

    Atualizacao appspec.yml

--- scripts/mount_filesystem.sh
+++ scripts/mount_filesystem.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 # Executar o comando mount -a para montar os sistemas de arquivos
+systemctl start apache2
 rm -rf /var/www/html/index.html
 chown -R www-data:www-data /var/www/html
 chmod -R 755 /var/www/html
--- scripts/unzip_artifact.sh
+++ scripts/unzip_artifact.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 # Desmontar o diretório /var/www/html/wp-content se ele estiver montado
+systemctl stop apache2
 umount -a
 
 # Remover arquivos existentes em /var/www/html/
