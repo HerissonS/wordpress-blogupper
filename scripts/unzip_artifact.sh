8eedf8462e2a1f59113a582ec44fadaa0ca9b403
HerissonS <antonioherisson@gmail.com>
2024-09-18 23:33:52 -0300

    Atualizacao appspec.yml

--- scripts/unzip_artifact.sh
+++ scripts/unzip_artifact.sh
@@ -2,7 +2,7 @@
 
 # Desmontar o diretório /var/www/html/wp-content se ele estiver montado
 systemctl stop apache2
-umount -a
+umount /var/www/html/wp-content
 
 # Remover arquivos existentes em /var/www/html/
 rm -rf /var/www/html/*
